8a5b811f92b15e428364c9c22576d7c9f07cbde8
DCP529 <61003286+DCP529@users.noreply.github.com>
2021-07-18 22:49:10 +0300

    Добавьте файлы проекта.

---
+++ Training/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(NewMethod));
+            thread1.Start();
+
+            MethodAsync();
+
+            Console.ReadLine();
+        }
+
+
+        static void NewMethod()
+        {
+            int j = 0;
+
+            for (int i = 0; i < 15; i++)
+            {
+                j++;
+                Console.WriteLine(j);
+            }
+        }
+
+        static async Task MethodAsync()
+        {
+            await Task.Run(() => NewMethod());
+            Console.WriteLine("It's so fucking deep");
+        }
+    }
+}
